c22148e42d0ce96c310284baa75111d178cc7e23
untitled <62313077+Beyzay@users.noreply.github.com>
2025-10-01 19:02:18 -0400

    Add NavBar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+
 
 export default function Home() {
   
   return (
     <div>
+
+      {/* Navigation Menu */}
+      <NavBar />
+      
     </div>
   )
 }
